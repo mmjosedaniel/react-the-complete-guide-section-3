e6385188d2e39e658fd2b809fddb9feee7701505
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-01-19 19:13:18 -0500

    30. Understanding JSX

--- react-complete-gide/src/App.js
+++ react-complete-gide/src/App.js
@@ -8,6 +8,8 @@ class App extends Component {
         <h1>Hi! I'm React</h1>
       </div>
     );
+    // Delete the next code from the lesson 30. Understanding JSX when starting the next lesson:
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Hi, I'm a React app!!!"));
   }
 }
 
