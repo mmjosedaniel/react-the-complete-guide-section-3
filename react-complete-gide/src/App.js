c4fb9a3637d76353ff47034785c8e09261cb850b
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-01-23 22:29:13 -0500

    Understanding and Usin State

--- react-complete-gide/src/App.js
+++ react-complete-gide/src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi! I'm React</h1>
         <p>This is really working</p>
-        <Person name="Max" age="28" />
-        <Person name="Manu" age="29" >My hobbies: Racing</Person>
-        <Person name="Stephanie" age="26" />
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Hi, I'm a React app!!!"));
