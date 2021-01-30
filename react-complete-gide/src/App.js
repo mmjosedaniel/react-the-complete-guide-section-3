62af55f02e9f35445e2a0fde5132ca56c3373431
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-01-30 15:52:07 -0500

    52. Rendering Content Conditionally (Not working, I will restart the porject)

--- react-complete-gide/src/App.js
+++ react-complete-gide/src/App.js
@@ -8,10 +8,14 @@ const app = props => {
       { name: 'Max', age: 28 },
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 26 }
-    ]
+    ],
+    otherState: 'some othe value',
+    showPerson: false
   });
 
   const [otherState, setOtherState] = useState('some othe value');
+
+  const [showPersonState, setShowPersonState] = useState({showPerson: false});
 
   console.log(personsState, otherState);
 
@@ -37,6 +41,11 @@ const app = props => {
      });
   };
 
+  const togglePersonsHandler = () => {
+    const doesShow = showPersonState.showPerson;
+    setShowPersonState({showPersons: !doesShow});
+  };
+
   const style = {
     backgroundColor: 'white',
     font: 'inherit',
@@ -51,20 +60,26 @@ const app = props => {
       <p>This is really working</p>
       <button 
         style={style}
-        onClick={() => switchNameHandler('Maximilin!!')}>Switch Name</button>
-      <Person
-        name={personsState.persons[0].name}
-        age={personsState.persons[0].age}
-      />
-      <Person 
-      name={personsState.persons[1].name}
-      age={personsState.persons[1].age} 
-      click={switchNameHandler.bind(this, 'Max!')}
-      changed={nameChangeHandler} >My hobbies: Racing</Person>
-      <Person
-        name={personsState.persons[2].name}
-        age={personsState.persons[2].age}
-      />
+        onClick={togglePersonsHandler}>Switch Name
+      </button>
+      {
+        showPersonState.showPerson === true ?
+        <div>
+          <Person
+            name={personsState.persons[0].name}
+            age={personsState.persons[0].age}
+          />
+          <Person 
+          name={personsState.persons[1].name}
+          age={personsState.persons[1].age} 
+          click={switchNameHandler.bind(this, 'Max!')}
+          changed={nameChangeHandler} >My hobbies: Racing</Person>
+          <Person
+            name={personsState.persons[2].name}
+            age={personsState.persons[2].age}
+          />
+        </div> : null         
+      }
     </div>
   );
   // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Hi, I'm a React app!!!"));
